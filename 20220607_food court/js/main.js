b88d86401140d295250a03e5f90176cd2d2863a1
ysy7 <w2105@e-mirim.hs.kr>
2022-06-14 14:59:07 +0900

    js 여러가지 함수 모습 복습

---
+++ 20220607_food court/js/main.js
@@ -0,0 +1,11 @@
+// function say(){
+//     console.log("helo world");
+// }
+
+// const say = function(){
+//     console.log("hello world2");
+// }
+// say();
+
+const say= () => console.log("hello world3");
+say();

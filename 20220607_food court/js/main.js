8ed55a918ecf2db87bb99ccf1c63382b7894fd66
ysy7 <w2105@e-mirim.hs.kr>
2022-06-14 15:16:36 +0900

    변경댐 뭐 지움

--- 20220607_food court/js/main.js
+++ 20220607_food court/js/main.js
@@ -1,3 +1,21 @@
+//toggle hidden menu
+function toggleMenu(toggleId, navListId){
+    const toggle = document.getElementById(toggleId);
+    const navList = document.getElementById(navListId);
+
+    function clickHandler(){
+        navList.
+    }
+
+    if(toggle && navList){
+        toggle.addEventListener('click', )
+    }
+
+}
+toggleMenu('nav-toggle','nav-list');
+
+
+
 // function say(){
 //     console.log("helo world");
 // }
@@ -7,5 +25,6 @@
 // }
 // say();
 
-const say= () => console.log("hello world3");
-say();
+// const say= () => console.log("hello world3");
+// say();
+
